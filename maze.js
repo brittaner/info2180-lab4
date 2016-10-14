f68079c9ebe6cd5e2aa8c59603d248e78f5c0080
Brittane Reid <Brittane Reid>
2016-10-14 15:54:04 -0500

    changes to exercise 2

--- maze.js
+++ maze.js
@@ -7,13 +7,18 @@ window.onload = function() //executes function after window has loaded
 
 	//}
 	var boundaries = document.querySelectorAll(".boundary");       //select all boundary classes and stores in variable
-		for (counter=0; counter < boundaries.length-1; counter++)  // loops through all the divs
+		for (var counter=0; counter < boundaries.length-1; counter++)  // loops through all the divs
 		{
 			boundaries[counter].addEventListener("mouseover",redWalls);             // listens for mouse to touch walls and applies redWalls function
 		}
-			function redWalls()                                            // function to turn walls red if first wall is touched
-			{
-				boundaries.setAttribute("class","youlose");
-			}
+	function redWalls()                                            // function to turn walls red if first wall is touched
+	{
+		for (var redcount= 0; redcount < boundaries.length-1; redcount++)
+		{
+				boundaries[redcount].setAttribute('style','background-color:#ff8888');
+		}
+			
+
+	}
 
 }
