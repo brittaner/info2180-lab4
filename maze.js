725bab6ac538eba8e2bb880e5106b3517e9fdfc8
brittaner <coyreid@hotmail.com>
2016-10-14 14:36:27 -0500

    Add files via upload

---
+++ maze.js
@@ -0,0 +1,11 @@
+window.onload = function() //executes function after window has loaded
+{
+	//var firstwall= document.getElementById(boundary1);
+	//firstwall.addEventListener("mouseover", function(){
+		//firstwall.setAttribute("style","background-color: #ff8888;");
+		//firstwall.className="youlose";
+
+	//}
+	var boundaries = document.querySelector(".boundary");
+		boundaries[0].setAttribute("class","youlose");
+}
